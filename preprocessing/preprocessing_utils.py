472f84b6a88f63b8b18b983e4af49b0ef271da91
Emil Korczak <ekor@itu.dk>
2025-10-23 15:29:54 +0200

    Added Preprocessing

--- preprocessing/preprocessing_utils.py
+++ preprocessing/preprocessing_utils.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn.inspection import permutation_importance
+import statsmodels.api as sm
 
 def clean_data(df, drop_exp_above_1=True):
     if drop_exp_above_1:
@@ -7,16 +10,26 @@ def clean_data(df, drop_exp_above_1=True):
     df = df.dropna()
     return df
 
-def encode_categoricals(df, one_hot=True):
-    cat_cols = ["VehBrand", "VehGas", "Area", "Region"]
-    if one_hot:
-        df = pd.get_dummies(df, columns=cat_cols, drop_first=True)
-    else:
-        for c in cat_cols:
-            df[c] = df[c].astype("category").cat.codes
+def feature_selection(df):
+    df = df.drop(columns=["IDpol", "Density"])
+    
+    return df
+def encode_categoricals(df):
+    cat_cols = ["Area", "VehBrand", "VehGas", "Region"]
+    
+    df = pd.get_dummies(df, columns=cat_cols, drop_first=True)
     return df
 
 def scale_features(df, features):
     scaler = StandardScaler()
     df[features] = scaler.fit_transform(df[features])
     return df, scaler
+
+train = pd.read_csv("../data/claims_train.csv")
+train = feature_selection(train)
+train = scale_features(train, features=["VehPower", "VehAge", "DrivAge", "BonusMalus"])[0]
+train = encode_categoricals(train)
+train = clean_data(train)
+train
+
+
